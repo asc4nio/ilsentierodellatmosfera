1f4ff0262709771db42f4e6bd4a0c5fdf503e6ea
asc4nio <ascaniopaonetti@gmail.com>
2024-11-05 22:23:18 +0100

    cleaner resize handle

--- src/App.vue
+++ src/App.vue
@@ -7,42 +7,17 @@ import { AppState } from "./stores/appStore";
 import { DeviceState } from "./stores/deviceStore";
 import { onMounted } from "vue";
 
-onMounted(async () => {
+import { updateSize } from "./components/utils";
+
+function handleSize() {
   DeviceState.updateInfo();
-  scaleMainContainer();
-});
-
-// Dimensioni di riferimento
-const BASE_WIDTH = 1920;
-const BASE_HEIGHT = 1080;
-
-function scaleMainContainer() {
-  const mainContainer = document.getElementById("main");
-
-  // Calcola il fattore di scala basato sulle dimensioni della finestra
-  const scaleX = window.innerWidth / BASE_WIDTH;
-  const scaleY = window.innerHeight / BASE_HEIGHT;
-  const scale = Math.min(scaleX, scaleY);
-
-  // Applica le dimensioni scalate in pixel al contenitore
-  mainContainer.style.width = `${BASE_WIDTH * scale}px`;
-  mainContainer.style.height = `${BASE_HEIGHT * scale}px`;
-
-  // Centra il contenitore all'interno della viewport
-  mainContainer.style.top = `${
-    (window.innerHeight - BASE_HEIGHT * scale) / 2
-  }px`;
-  mainContainer.style.left = `${
-    (window.innerWidth - BASE_WIDTH * scale) / 2
-  }px`;
-
-  // Ridimensiona il font in base alla scala
-  document.body.style.fontSize = `${16 * 1.25 * scale}px`;
+  updateSize();
 }
 
-// // Chiama la funzione al caricamento e al ridimensionamento della finestra
-// window.addEventListener('load', scaleMainContainer);
-window.addEventListener("resize", scaleMainContainer);
+onMounted(async () => {
+  handleSize();
+  window.addEventListener("resize", handleSize);
+});
 </script>
 
 <template>
@@ -56,8 +31,7 @@ window.addEventListener("resize", scaleMainContainer);
 <style scoped>
 main {
   @apply absolute;
-  /* @apply relative h-full w-full; */
-  @apply border border-black border-[.1em];
-  @apply bg-white;
+  /* @apply bg-white; */
+  @apply border border-black;
 }
 </style>
--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,6 +1,3 @@
-/* Get the documentElement (<html>) to display the page in fullscreen */
-// var elem = document.documentElement;
-
 /* View in fullscreen */
 export function openFullscreen() {
   let elem = document.documentElement;
@@ -27,4 +24,32 @@ export function closeFullscreen() {
     /* IE11 */
     document.msExitFullscreen();
   }
+}
+
+export function updateSize() {
+  // Dimensioni di riferimento
+  const BASE_WIDTH = 1920;
+  const BASE_HEIGHT = 1080;
+
+  const mainContainer = document.getElementById("main");
+
+  // Calcola il fattore di scala basato sulle dimensioni della finestra
+  const scaleX = window.innerWidth / BASE_WIDTH;
+  const scaleY = window.innerHeight / BASE_HEIGHT;
+  const scale = Math.min(scaleX, scaleY);
+
+  // Applica le dimensioni scalate in pixel al contenitore
+  mainContainer.style.width = `${BASE_WIDTH * scale}px`;
+  mainContainer.style.height = `${BASE_HEIGHT * scale}px`;
+
+  // Centra il contenitore all'interno della viewport
+  mainContainer.style.top = `${
+    (window.innerHeight - BASE_HEIGHT * scale) / 2
+  }px`;
+  mainContainer.style.left = `${
+    (window.innerWidth - BASE_WIDTH * scale) / 2
+  }px`;
+
+  // Ridimensiona il font in base alla scala
+  document.body.style.fontSize = `${16 * 1.25 * scale}px`;
 }
